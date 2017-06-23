9310fdd6837af03cce0c70a8e41b0f9cf2d00f8c
ikhlaspm12 <ikhlaspm12@hotmail.com>
2017-06-23 00:35:01 -0500

    this is my first project

---
+++ test4/src/main/java/com/mycompany/test4/Workers.java
@@ -0,0 +1,30 @@
+package com.mycompany.test4;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ikhlas
+ */
+public class Workers {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Workers{" + "name=" + name + '}';
+    }
+    
+    
+}
